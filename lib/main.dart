2dc5f6ee0a3bedd1b8cb72be404d3aef6ba53b06
patilnh <st186146@stud.uni-stuttgart.de>
2024-05-02 22:50:51 +0200

    added performance overlay

--- lib/main.dart
+++ lib/main.dart
@@ -5,20 +5,41 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _showPerformanceOverlay = false;
+
+  void _togglePerformanceOverlay() {
+    setState(() {
+      _showPerformanceOverlay = !_showPerformanceOverlay;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Bouncing',
+      title: 'Bouncing Balls',
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Bouncing Balls'),
+          title: Text(''),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.assessment),
+              onPressed: _togglePerformanceOverlay,
+            )
+          ],
         ),
         body: ContainerWidget(),
       ),
+      debugShowCheckedModeBanner: false,
+      showPerformanceOverlay: _showPerformanceOverlay,
     );
   }
 }
